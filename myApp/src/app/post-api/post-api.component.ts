bf4e4fcd35fa8a9aa6628fa2103d45100e61ca88
rajeev <rajivg216@gmail.com>
2025-01-14 15:03:18 +0530

    Put and Delete API Added

--- myApp/src/app/post-api/post-api.component.ts
+++ myApp/src/app/post-api/post-api.component.ts
@@ -1,44 +1,96 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject , OnInit } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-api',
   imports: [FormsModule],
   templateUrl: './post-api.component.html',
-  styleUrl: './post-api.component.css'
+  styleUrls: ['./post-api.component.css'] 
 })
-export class PostApiComponent implements OnInit{
-  
-  deptObj: any = {
-    "departmentId": 0,
-    "departmentName": "",
-    "departmentLogo": ""
+export class PostApiComponent implements OnInit {
+  deptObj: { departmentId: number; departmentName: string; departmentLogo: string } = {
+    departmentId: 0,
+    departmentName: '',
+    departmentLogo: ''
   };
 
-  deptList:any[] = [];
-  
-  // It will execute when the component is loaded.
+  deptList: any[] = [];
+
+  http = inject(HttpClient); // Injecting HttpClient instance
+
+  // Executes when the component is loaded
   ngOnInit() {
     this.getDepartment();
   }
-  
-  // Creating an instance of HttpClient through dependency injection.
-  http = inject(HttpClient);
 
+  // Save department
   saveDept() {
-    this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment", this.deptObj).subscribe((res: any) => {
-      if (res.result) {
-        alert("Department added successfully");
-      } else {
-        alert(res.message);
-      }
-    });
+    this.http
+      .post('https://projectapi.gerasim.in/api/Complaint/AddNewDepartment', this.deptObj)
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert('Department added successfully');
+          this.resetForm(); // Clear the form after saving
+          this.getDepartment(); // Refresh the department list
+        } else {
+          alert(res.message);
+        }
+      });
   }
 
+  // Get department
   getDepartment() {
-    this.http.get('https://projectapi.gerasim.in/api/Complaint/GetParentDepartment').subscribe((res: any) => {
-      this.deptList = res.data;
-    });
+    this.http
+      .get('https://projectapi.gerasim.in/api/Complaint/GetParentDepartment')
+      .subscribe((res: any) => {
+        this.deptList = res.data;
+      });
+  }
+
+  // Delete department
+  onDelete(id: number) {
+    const isDelete = confirm('Are you sure you want to delete this department?');
+    if (isDelete) {
+      this.http
+        .delete(`https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId?departmentId=${id}`)
+        .subscribe((res: any) => {
+          if (res.result) {
+            alert('Department deleted successfully');
+            this.getDepartment(); // Refresh the department list
+          } else {
+            alert(res.message);
+          }
+        });
+    }
+  }
+
+  // Edit department
+  onEdit(data: any) {
+    this.deptObj = data;
+  }
+
+  // Update department
+  updateDept() {
+    this.http
+      .put('https://projectapi.gerasim.in/api/Complaint/UpdateDepartment', this.deptObj)
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert('Department updated successfully');
+          this.resetForm(); // Clear the form after updating
+          this.getDepartment(); // Refresh the department list
+        } else {
+          alert(res.message);
+        }
+      });
+  }
+
+  // Clear the form
+  resetForm() {
+    this.deptObj = {
+      departmentId: 0,
+      departmentName: '',
+      departmentLogo: ''
+    };
   }
 }
