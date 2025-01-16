fea362a5868763f246fa78d0112abe9d6543af7b
rajeev <rajivg216@gmail.com>
2025-01-16 18:08:14 +0530

    Communication between two siblings via Parent

--- ioApp/src/app/child/child.component.ts
+++ ioApp/src/app/child/child.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -21,23 +21,50 @@ export class ChildComponent {
   //   this.messageSent.emit('Hello from the child!'); // Emit the message when the button is clicked
   // }
 
-    // @Input() items : string[] = []; // Receiving the items from the parent.
+  // @Input() items : string[] = []; // Receiving the items from the parent.
 
-      // @Input() users !: { name : string, gender : string };
+  // @Input() users !: { name : string, gender : string };
 
-      // @Output() dataSent = new EventEmitter<{ username : string, email : string}>();
+  // @Output() dataSent = new EventEmitter<{ username : string, email : string}>();
 
-      // sendData() {
-      //   this.dataSent.emit({ username : 'rajeev29', email : 'rajeev29@gmail.com'});
-      // }
+  // sendData() {
+  //   this.dataSent.emit({ username : 'rajeev29', email : 'rajeev29@gmail.com'});
+  // }
 
-    @Input() data !: string;   // Receiving data from parent.
-    
-    @Output() dataChange = new EventEmitter<string>();  // Emit changes to parent
+  // @Input() data !: string;   // Receiving data from parent.
 
-    // method to change the data send it back to the parent
-    updateData(newData : string){
-      this.data = newData;
-      this.dataChange.emit(this.data);
-    }
+  // @Output() dataChange = new EventEmitter<string>();  // Emit changes to parent
+
+  // // method to change the data send it back to the parent
+  // updateData(newData : string){
+  //   this.data = newData;
+  //   this.dataChange.emit(this.data);
+  // }
+
+  // @Input() username !: string;
+
+  // ngOnChanges(changes : SimpleChanges){
+  //   if(this.username && this.username.length < 3){
+  //     this.username = "Default UserName";
+  //   }else{
+  //     this.username = this.username;
+  //   }
+  // };
+
+  // @Output() usernameChanged = new EventEmitter<string>;
+  // @Output() statusChanged = new EventEmitter<string>;
+
+  // sendUserName(){
+  //   this.usernameChanged.emit('Updated UserName.');
+  // }
+
+  // sendStatus(){
+  //   this.statusChanged.emit('Active');
+  // }
+
+  @Output() sendData = new EventEmitter<string>();
+
+  emitData(){
+    this.sendData.emit('Data from child one.')
+  }
 }
--- ioApp/src/app/reusableComponent/alert/alert.component.ts
+++ ioApp/src/app/reusableComponent/alert/alert.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { ChildComponent } from '../../child/child.component';
+import { ChildTwoComponent } from '../../child/child-two/child-two.component';
 
 @Component({
   selector: 'app-alert',
-  imports: [ChildComponent],
+  imports: [ChildComponent, ChildTwoComponent],
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.css'
 })
@@ -18,25 +19,44 @@ export class AlertComponent {
   //   this.message = message;
   // }
 
-    // list = ['Item1','Item2','Item3'];
+  // list = ['Item1','Item2','Item3'];
 
-    // updateItems(){
-    //   this.list.push('New Item');
-    // }
+  // updateItems(){
+  //   this.list.push('New Item');
+  // }
 
-    // user = {
-    //   name : 'rajeev',
-    //   gender : 'male'
-    // };
+  // user = {
+  //   name : 'rajeev',
+  //   gender : 'male'
+  // };
 
-    // username : string = "";
-    // email : string = "";
+  // username : string = "";
+  // email : string = "";
 
-    // handleData(Data : { username : string, email : string }){
-    //   this.username = Data.username;
-    //   this.email = Data.email;
-    // }
+  // handleData(Data : { username : string, email : string }){
+  //   this.username = Data.username;
+  //   this.email = Data.email;
+  // }
 
-    parentData : string = 'initial data.';
+  // parentData : string = 'initial data.';
+
+  // username: string = 'Rajeev';
+
+  // receivedUsername : string = '';
+  // receivedStatus : string = '';
+
+  // handleUserNameChange(newUserName : string){
+  //   this.receivedUsername = newUserName;
+  // }
+
+  // handleStatusChange(newStatus : string){
+  //   this.receivedStatus = newStatus;
+  // }
+
+  siblingData : string = '';  // Data to pass between siblings.
+
+  handleSiblingData(data : string){
+    this.siblingData = data;       // Update Parent state with data from sibling 1
+  }
 
 }
